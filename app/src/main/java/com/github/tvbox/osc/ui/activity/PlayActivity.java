3b4bbb8ce5bfdffa2c292341e6b20fb13f2ead1d
ShawnBenC <101103821+ShawnBenC@users.noreply.github.com>
2022-08-13 17:36:28 +0800

    重播,if清除现有进度

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -479,6 +479,8 @@ public class PlayActivity extends BaseActivity {
 
         playUrl(null, null);
         String progressKey = mVodInfo.sourceKey + mVodInfo.id + mVodInfo.playFlag + mVodInfo.playIndex;
+        //重新播放清除现有进度
+        if (reset) {CacheManager.delete(MD5.string2MD5(progressKey), 0);}
         if (Thunder.play(vs.url, new Thunder.ThunderCallback() {
             @Override
             public void status(int code, String info) {
